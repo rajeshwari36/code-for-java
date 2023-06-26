4e04b25f8bff2651b3351d6ae1234765c6cbfd1d
Rajeshwari Kolhe <107494495+rajeshwari36@users.noreply.github.com>
2023-06-26 18:48:05 +0530

    Add files via upload

---
+++ code.java
@@ -0,0 +1,30 @@
+
+public class code{
+
+public static void main(String args[]){
+
+int arr[]={34,56,77,89,54};
+
+int sum=0;
+int avg;
+
+for(int i=0; i<arr.length; i++){
+
+sum=sum+arr[i];
+
+
+
+}
+System.out.println(sum);
+
+System.out.println(avg=sum/arr.length);
+
+
+
+
+
+}
+
+
+
+}
---
+++ countNo.java
@@ -0,0 +1,23 @@
+public class countNo{
+
+
+public static void main(String[] args){
+
+int a = 1998;
+
+int count=0;
+
+while(a>0){
+
+a=a/10;
+
+count++;
+
+}
+
+
+
+System.out.println(count);
+}
+
+}
